bcf2eac6384aac216bb2b6258bdb7e22d181bebc
SomeshN-GitHub <63358076+SomeshN-GitHub@users.noreply.github.com>
2022-04-23 18:45:32 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ window.onclick =(event)=>{
     if (!event.target.matches('.menu_link')){
         if(event.target.id == 'menu'){
             event.target.src = event.target.src.includes('close') ?
-         './images/icon-menu.svg' : '/images/icon-close-menu.svg';
+         './images/icon-menu.svg' : './images/icon-close-menu.svg';
          document.getElementsByClassName('nav_links_container')[0].classList.toggle("show");
          document.getElementsByTagName('body')[0].classList.toggle('body_overlay');
         }
@@ -11,15 +11,15 @@ window.onclick =(event)=>{
             element.classList.remove("show");
         });
         Array.from(document.getElementsByClassName("menu_link")).forEach(element => {
-            element.firstElementChild.src = "/images/icon-arrow-down.svg";
+            element.firstElementChild.src = "./images/icon-arrow-down.svg";
         });
         
     }else {
         event.target.children[1].classList.toggle("show");
         event.target.firstElementChild.src =
         event.target.firstElementChild.src.includes('up') ? 
-        '/images/icon-arrow-down.svg' :
-        '/images/icon-arrow-up.svg';
+        './images/icon-arrow-down.svg' :
+        './images/icon-arrow-up.svg';
     }
 }
 
